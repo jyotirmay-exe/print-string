77aa22fa5a3957f24b9dced8f52060c596789a1f
Jyotirmay Kumar Jha <jyotirmay18.jk@gmail.com>
2022-04-16 16:54:23 +0530

    init

---
+++ run.py
@@ -0,0 +1,11 @@
+from random import randint
+import os, time
+
+os.system('cls')
+
+st = "Hello World"
+
+def Main(string):
+    while True:
+        print(chr(randint(65,122)))
+        time.sleep(1)
