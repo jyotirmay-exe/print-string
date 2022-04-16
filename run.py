70c93fbb481c96062251f6b45994eff7b4687b44
Jyotirmay Kumar Jha <jyotirmay18.jk@gmail.com>
2022-04-16 17:14:14 +0530

    randomize

--- run.py
+++ run.py
@@ -7,13 +7,17 @@ st = "Hello World"
 
 def Main(string:str):
     newStr = ""
-    print(newStr)
     if len(newStr)==0:
         newStr = string[0]
     while True:
+        time.sleep(0.2)
         ch = chr(randint(65,122))
         if string.startswith(newStr+ch):
             newStr+=ch
-            print(newStr)
-            time.sleep(0.2)
+            print("\r"+newStr,end="")
+            print()
+        else:
+            print("\r"+newStr+ch,end="")
+        if newStr==string:
+            break
 Main("Hello")
