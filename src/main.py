2f564ae49036fab49a2d153f9260a69c9074263e
Jyotirmay Kumar Jha <jyotirmay18.jk@gmail.com>
2022-04-17 16:46:05 +0530

    remove newline

--- src/main.py
+++ src/main.py
@@ -5,17 +5,18 @@ def Main(string:str):
     newStr = ""
     ind = 0
     while True:
-        time.sleep(0.2)
+        time.sleep(0.05)
         #generate a random printable ascii char
         ch = chr(randint(65,122))
         #test patience
         if string.startswith(newStr+ch):
             newStr+=ch
-            print("\r"+newStr)
+            print("\r"+newStr,end="")
             ind+=1
             try:
                 if ord(string[ind])==32:
                     newStr+=" "
+                    ind += 1
             except IndexError:
                 pass
         else:
